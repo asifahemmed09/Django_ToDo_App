19f3ca916ba5cc33f872744f074329225ccd4e19
asifahemmed09 <asifahemmed.dev@gmail.com>
2026-07-08 01:01:08 +0600

    fetch tasks in home page

--- todo/models.py
+++ todo/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 # Create your models here.
-class Todo(models.Model):
+class Task(models.Model):
     task = models.CharField(max_length=250)
     is_completed = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
